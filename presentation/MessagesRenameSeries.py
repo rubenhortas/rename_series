a45e37e9112dacd3abcc7bd2f745e3016f9055a2
ruben <ruben>
2015-05-23 19:55:44 +0200

    changed 'check_for_duplicated' to 'look_for_duplicated_episodes'

--- presentation/MessagesRenameSeries.py
+++ presentation/MessagesRenameSeries.py
@@ -6,7 +6,7 @@
 @contact: rubenhortas at gmail.com
 @github:  http://github.com/rubenhortas
 @license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
-@file:    MessagesSeries.py
+@file:    MessagesRenameSeries.py
 """
 
 from .Color import Color
