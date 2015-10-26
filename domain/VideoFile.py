53371359ed2b9c52c70c77f2820762e46a549c4f
rubenhortas <rubenhortas@gmail.com>
2015-10-26 23:42:32 +0100

    Changed comparison. Added The Middle to the list.

--- domain/VideoFile.py
+++ domain/VideoFile.py
@@ -20,70 +20,73 @@ from .File import File
 
 EXPANDED_NAMES = {
     # American Horror Story
-    "AmericanHStory": "American Horror Story",
-    "AHS":            "American Horror Story",
-    "AmericanStory":  "American Horror Story",
+    "americanhstory": "American Horror Story",
+    "ahs":            "American Horror Story",
+    "americanstory":  "American Horror Story",
 
     # Arrow
-    "Arr": "Arrow",
+    "arr": "Arrow",
 
     # Bates Motel
-    "BMotel":        "Bates Motel",
-    "BaMotel":       "Bates Motel",
-    "BatesMotel":    "Bates Motel",
+    "bmotel":        "Bates Motel",
+    "bamotel":       "Bates Motel",
+    "batesmotel":    "Bates Motel",
 
     # Boardwalk Empire
-    "BoardEmpire":       "Boardwalk Empire",
-    "BoardwalkEmpire":   "Boardwalk Empire",
-    "BoarEmpire":        "Boardwalk Empire",
+    "booardempire":       "Boardwalk Empire",
+    "boardwalkempire":   "Boardwalk Empire",
+    "boarempire":        "Boardwalk Empire",
 
     # Bob"s Burgers
-    "Bobs Burgers":  "Bob\"s Burgers",
+    "bobs burgers":  "Bob\"s Burgers",
 
     # Castle
-    "Cas": "Castle",
+    "cas": "Castle",
 
     # Doctor Who
-    "DoctorWho":     "Doctor Who",
+    "doctorwho":     "Doctor Who",
 
     # Érase una vez
-    "EraseVez":      "Érase una vez (Once upon a time)",
-    "Erase Una Vez": "Érase una vez (Once upon a time)",
+    "erasevez":      "Érase una vez (Once upon a time)",
+    "erase una vez": "Érase una vez (Once upon a time)",
 
     # El mentalista
-    "EMentalista":   "El mentalista",
-    "Mentalista":    "El mentalista",
+    "ementalista":   "El mentalista",
+    "mentalista":    "El mentalista",
 
     # Elementary
-    "Ele":      "Elementary",
-    "Elem":     "Elementary",
-    "Eleme":    "Elementary",
-    "Elmntry":  "Elementary",
+    "ele":      "Elementary",
+    "elem":     "Elementary",
+    "eleme":    "Elementary",
+    "elmntry":  "Elementary",
 
     # Marvel"s Agents of SHIELD
-    "Marvel\"s Agents of S H I E L D":   "Marvel\"s Agents of S.H.I.E.L.D.",
-    "Marvels Agents":                    "Marvel\"s Agents of S.H.I.E.L.D.",
+    "marvel\"s agents of s h i e l d":   "Marvel\"s Agents of S.H.I.E.L.D.",
+    "marvels agents":                    "Marvel\"s Agents of S.H.I.E.L.D.",
 
 
     # Ray Donovan
-    "RDonovan": "Ray Donovan",
+    "rdonovan": "Ray Donovan",
 
     # South Park
-    "SP":        "South Park",
-    "SPark":     "South Park",
-    "SouthPark": "South Park",
+    "sp":        "South Park",
+    "spark":     "South Park",
+    "southpark": "South Park",
 
     # The Big Bang Theory
-    "TBBTheory":     "The Big Bang Theory",
-    "TBiBaTheory":   "The Big Bang Theory",
+    "tbbtheory":     "The Big Bang Theory",
+    "tbibatheory":   "The Big Bang Theory",
 
+    # The Middle
+    "tmidd":        "The Middle",
+    "tmid":         "The Middle",
     # The Walking Dead
-    "TWalkDead":     "The Walking Dead",
-    "TWalkingDead":  "The Walking Dead",
+    "twalkdead":     "The Walking Dead",
+    "twalkingdead":  "The Walking Dead",
 
     # True Detective
-    "TDetective":    "True detective",
-    "TrDetective":   "True detective",
+    "tdetective":    "True detective",
+    "trdetective":   "True detective",
 }
 
 EPISODE_TITLE_PATTERN = re.compile("[\w ]*", re.UNICODE)
@@ -108,9 +111,11 @@ class VideoFile(File):
         if self.debugging:
             print()
             Messages.debug_msg("VideoFile __init___")
-            Messages.debug_msg("\tself.f_absolute_original_path: {0}".format(self.f_abs_original_path))
+            Messages.debug_msg(
+                "\tself.f_absolute_original_path: {0}".format(self.f_abs_original_path))
             Messages.debug_msg("\tself.file_name: {0}".format(self.file_name))
-            Messages.debug_msg("\tis well formated: {0}".format(self.__is_well_formated()))
+            Messages.debug_msg(
+                "\tis well formated: {0}".format(self.__is_well_formated()))
 
         if not self.is_well_formated():
             self.__remove_quality()
@@ -144,9 +149,11 @@ class VideoFile(File):
 
             if self.debugging:
                 Messages.debug_msg("__get_episode")
-                Messages.debug_msg("\tself.episode_in_name: {0}".format(self.episode_in_name))
+                Messages.debug_msg(
+                    "\tself.episode_in_name: {0}".format(self.episode_in_name))
                 Messages.debug_msg("\tself.episode: {0}".format(self.episode))
-                Messages.debug_msg("\tself.new_file_name: {0}".format(self.new_file_name))
+                Messages.debug_msg(
+                    "\tself.new_file_name: {0}".format(self.new_file_name))
 
     def __is_serie(self):
         """
@@ -160,26 +167,26 @@ class VideoFile(File):
             return False
 
     def __get_show_name(self):
-            """
-            __get_show_name(self)
-                Gets the show name and if it's in original version.
-            """
+        """
+        __get_show_name(self)
+            Gets the show name and if it's in original version.
+        """
 
-            self.extension = os.path.splitext(self.file_name)[1]
-            file_name = os.path.splitext(self.file_name)[0]
+        self.extension = os.path.splitext(self.file_name)[1]
+        file_name = os.path.splitext(self.file_name)[0]
 
-            list_file_name = file_name.split(self.episode_in_name)
+        list_file_name = file_name.split(self.episode_in_name)
 
-            show_name = list_file_name[0]
-            show_name = show_name.replace(".", " ")
-            show_name = show_name.strip()
+        show_name = list_file_name[0]
+        show_name = show_name.replace(".", " ")
+        show_name = show_name.strip()
 
-            self.show_name = show_name
+        self.show_name = show_name
 
-            # DEBUG
-            if self.debugging:
-                Messages.debug_msg("__get_show_name")
-                Messages.debug_msg("\tself.show_name: {0}".format(self.show_name))
+        # DEBUG
+        if self.debugging:
+            Messages.debug_msg("__get_show_name")
+            Messages.debug_msg("\tself.show_name: {0}".format(self.show_name))
 
     def __expand_show_name(self):
         """
@@ -187,37 +194,40 @@ class VideoFile(File):
             Expands some serie titles.
         """
 
-        if self.show_name in EXPANDED_NAMES:
-            self.show_name = EXPANDED_NAMES.get(self.show_name)
+        if self.show_name.lower() in EXPANDED_NAMES:
+            self.show_name = EXPANDED_NAMES.get(self.show_name.lower())
 
             if self.debugging:
                 Messages.debug_msg("__expand_show_name")
-                Messages.debug_msg("\texpanded_name: {0}".format(self.show_name))
+                Messages.debug_msg(
+                    "\texpanded_name: {0}".format(self.show_name))
         else:
             if self.debugging:
-                Messages.debug_msg("{0} will be not expanded".format(self.show_name))
+                Messages.debug_msg(
+                    "{0} will be not expanded".format(self.show_name))
 
     def __get_episode_title(self):
 
-            file_name = os.path.splitext(self.file_name)[0]
+        file_name = os.path.splitext(self.file_name)[0]
 
-            list_file_name = file_name.split(self.episode_in_name)
+        list_file_name = file_name.split(self.episode_in_name)
 
-            show_name = list_file_name[1]
+        show_name = list_file_name[1]
 
-            if show_name:
-                title_match = EPISODE_TITLE_PATTERN.search(show_name)
+        if show_name:
+            title_match = EPISODE_TITLE_PATTERN.search(show_name)
 
-                if title_match:
-                    episode_title = title_match.group(0).strip()
+            if title_match:
+                episode_title = title_match.group(0).strip()
 
-                    if episode_title != "":
-                        self.episode_title = episode_title
+                if episode_title != "":
+                    self.episode_title = episode_title
 
-                        # DEBUG
-                        if self.debugging:
-                            Messages.debug_msg("__get_episode_title")
-                            Messages.debug_msg("\tself.episode_title: {0}".format(self.episode_title))
+                    # DEBUG
+                    if self.debugging:
+                        Messages.debug_msg("__get_episode_title")
+                        Messages.debug_msg(
+                            "\tself.episode_title: {0}".format(self.episode_title))
 
     def __set_ov(self):
         """
@@ -240,7 +250,8 @@ class VideoFile(File):
         if year_match:
             year_in_show_name = year_match.group(0).lstrip()
             new_year = "({0})".format(year_in_show_name)
-            self.show_name = self.show_name.replace(year_in_show_name, new_year)
+            self.show_name = self.show_name.replace(
+                year_in_show_name, new_year)
 
     def __set_show_name(self):
         """
@@ -254,7 +265,8 @@ class VideoFile(File):
             new_file_name = "{0} {1}".format(self.show_name, self.episode)
 
             if self.episode_title:
-                new_file_name = "{0} - {1}".format(new_file_name, self.episode_title)
+                new_file_name = "{0} - {1}".format(
+                    new_file_name, self.episode_title)
 
             if self.ov:
                 new_file_name = "{0} (VO)".format(new_file_name)
