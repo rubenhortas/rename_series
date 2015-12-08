3d6b352f6f178c51bf8c34010c0e6d40a1542008
rubenhortas <rubenhortas@gmail.com>
2015-12-08 11:23:40 +0100

    Added python utils

---
+++ application/utils/python_utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*
+
+"""
+@author:  Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact: rubenhortas at gmail.com
+@github:  http://github.com/rubenhortas
+@license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:    python_utils.py
+"""
+
+from sys import version_info
+from crosscutting import condition_messages
+
+
+def get_interpreter_version():
+    """
+    get_interpreter_version()
+        Gets the current version of the python interpreter.
+    """
+    major, minor, micro, release, serial = version_info
+    return major
+
+
+def exit_signal_handler(signal, frame):
+    """"
+    exit_signal_handler(signal, frame)
+        Hadles an exit signal.
+    Arguments:
+        - signal: (int) number of signal.
+        - frame: (string) name of the signal handler.
+    """
+
+    exit(0)
