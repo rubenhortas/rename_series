e9359adac70ca1b5b0a00ae9ebe84d1eba64b9d7
rubenhortas <rubenhortas@gmail.com>
2015-12-08 11:23:57 +0100

    Added constants

---
+++ crosscutting/constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*
+
+"""
+@author:      Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact:     rubenhortas at gmail.com
+@github:      http://github.com/rubenhortas
+@license:     CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:        constants.py
+@interpreter: python3
+"""
+
+REQUIRED_PYTHON_VERSION = 3
+SHOWS_PATHS = ['/home/ruben/Vídeos', '/home/ruben/Vídeos/temp']
+DIR_SEASON_NAME = "Temporada"
+FILE_WELL_FORMATED_PATTERN = re.compile(
+    "(?P<season>[\d]{1,2})x(?P<episode>[\d]{1,2})(?P<episode_title>[\w \-\(\)])?\.(?P<extension>[\w]{3})", re.UNICODE)
